22a723c4d648295e9189467d962f524c15a368c5
ZH-Matthew <m.zhitenev23@gmail.com>
2023-06-05 20:37:19 +0300

    Залил симпл код на мастер

---
+++ src/main/java/course2_5/employeeacc/EmployeeAccApplication.java
@@ -0,0 +1,13 @@
+package course2_5.employeeacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course2_5/employeeacc/EmployeeAccApplicationTests.java
@@ -0,0 +1,13 @@
+package course2_5.employeeacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
